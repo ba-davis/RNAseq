9d648ad23dbd43186d2f8edc02cd303946f87a5b
davibr <davibr@hoolock2.ohsu.edu>
2021-01-04 12:30:14 -0800

    added compile readcounts script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Code related to RNAseq analysis
+
+Strategy:
+Utilize FQC_Trim_align scripts to perform via SLURM:
+  - FastQC on raw files
+  - Trimmomatic
+  - FastQC on trimmed files
+  - alignment and gene counts via STAR and ensembl
+
+Produce a raw counts table
+  - given path to folder of ReadsPerGene.out.tab from STAR
+  
---
+++ compile_readcounts.R
@@ -0,0 +1,70 @@
+#!/usr/bin/Rscript
+
+# Take multiple ReadsPerGene.tab files from STAR and combine them into one table
+# We will select column 4 (stranded reverse) or column 2 (unstranded) read counts
+
+library(parallel)
+library(optparse)
+
+
+# Command Line Arguments
+# -d : path to directory of STAR's .ReadsPerGene files
+# -o : desired outfile (counts_table.txt)
+# -s : strand info, which column of STAR ReadsPerGene files to keep (usually 2 or 4)
+
+option_list = list(
+  make_option(c("-d", "--directory"), type="character", default=NULL,
+              help="path to ReadsPerGene files", metavar="character"),
+  make_option(c("-s", "--strand"), type="numeric", default=NULL,
+              help="number signifying which column of counts to keep", metavar="numeric"),
+  make_option(c("-o", "--outfile"), type="character", default="counts_table.txt",
+              help="desired outfile (counts_table.txt)", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+# Input Args Checks
+if (is.null(opt$directory)){
+  print_help(opt_parser)
+  stop("Missing input file directory path.n", call.=FALSE)
+}
+if (is.null(opt$strand)){
+  print_help(opt_parser)
+  stop("Missing strand column.n", call.=FALSE)
+}
+#if (is.null(opt$outfile)){
+#  print_help(opt_parser)
+#  stop("Missing output file name.n", call.=FALSE)
+#}
+
+# create a list that contains the path to all input files
+counts_list <- list.files(opt$directory, 'ReadsPerGene.out.tab$', full=T)
+names(counts_list) <- counts_list
+names(counts_list) <- gsub('.star.ReadsPerGene.out.tab$','',names(counts_list))
+names(counts_list) <- gsub(opt$directory, '',names(counts_list))
+names(counts_list) <- gsub("/", '',names(counts_list))
+
+# read in each counts file as a data frame and store in a list of df's
+counts_dfs <- mclapply(counts_list, function(x) read.table(x), mc.cores=8)
+
+# convert the correct counts column to numeric
+for (i in 1:length(counts_dfs)) {counts_dfs[[i]][,opt$strand] <- as.numeric(counts_dfs[[i]][,opt$strand])}
+
+# change the name of the correct column to be the sample name
+for (i in 1:length(counts_dfs)) {colnames(counts_dfs[[i]])[opt$strand] <- names(counts_dfs)[i]}
+
+# change the name of column 1 to "GeneID"
+for (i in 1:length(counts_dfs)) {colnames(counts_dfs[[i]])[1] <- "GeneID"}
+
+# drop unnecessary counts columns
+for (i in 1:length(counts_dfs)) {counts_dfs[[i]] <- subset(counts_dfs[[i]], select = colnames(counts_dfs[[i]][c(1,opt$strand)]))}
+
+# remove rows 1,2,3,4
+for (i in 1:length(counts_dfs)) {counts_dfs[[i]] <- counts_dfs[[i]][-c(1,2,3,4), ]}
+
+# merge all data frames on "GeneID"
+my_table <- Reduce(function(df1, df2) merge(df1, df2, by = "GeneID", all = TRUE), counts_dfs)
+
+# export the final table
+write.table(my_table, opt$outfile, col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
